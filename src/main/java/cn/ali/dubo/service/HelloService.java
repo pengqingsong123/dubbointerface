57610713f13ad7ce708f3d676428bc0f9c34f46f
helloworld <1025599740@qq.com>
2019-09-28 09:57:06 +0800

    add method

--- src/main/java/cn/ali/dubo/service/HelloService.java
+++ src/main/java/cn/ali/dubo/service/HelloService.java
@@ -1,9 +1,8 @@
 package cn.ali.dubo.service;
 
-import jdk.nashorn.internal.ir.annotations.Reference;
-
-
 public interface HelloService {
 
     String sayHello(String name);
+
+    String sayBey(String name);
 }
